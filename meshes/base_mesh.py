46884655fe5f86386ab98e1e213460cb2b616789
mnbvmnbv2 <ferdinand@ytteborg.com>
2024-04-20 22:28:04 +0200

    formatting

--- meshes/base_mesh.py
+++ meshes/base_mesh.py
@@ -9,15 +9,12 @@ class BaseMesh:
         self.attrs: tuple[str, ...] = None
         self.vao = None
 
-    def get_vertex_data(self) -> np.array:
-        ...
+    def get_vertex_data(self) -> np.array: ...
 
     def get_vao(self):
         vertex_data = self.get_vertex_data()
         vbo = self.ctx.buffer(vertex_data)
-        vao = self.ctx.vertex_array(
-            self.program, [(vbo, self.vbo_format, *self.attrs)], skip_errors=False
-        )
+        vao = self.ctx.vertex_array(self.program, [(vbo, self.vbo_format, *self.attrs)], skip_errors=True)
         return vao
 
     def render(self):
